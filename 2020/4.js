15c6ebf575e9cf5897dc232bf10480818de8c9e6
Daniel Robertson <danieljrobertson@gmail.com>
2020-12-05 18:19:02 +0000

    add solution for day 4 part 2

--- 2020/4.js
+++ 2020/4.js
@@ -65,6 +65,7 @@ hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in
 
 `
+const { includes } = require('./4data');
 const puzzleInput = require('./4data')
 
 /* I don't think this works. I think i have to build a crawler
@@ -76,9 +77,10 @@ console.log(inputParser(puzzleInput));  */
 
 // if its a character keep it. 
 
-const modifiedInput = puzzleInput.replace(/\n/g, '@').split('@@').join(`"},{"`).split('@').join(`","`).split(' ').join(`","`).split(':').join(`":"`).slice(3, -3);
-const passportsAsObjects = JSON.parse(`[${modifiedInput}]`)
-console.log(passportsAsObjects.length)
+const inputParser = (input) => JSON.parse(`[${input.replace(/\n/g, '@').split('@@').join(`"},{"`).split('@').join(`","`).split(' ').join(`","`).split(':').join(`":"`).slice(3, -3)}]`)
+
+// console.log(passportsAsObjects.length)
+
 const checkPassport = (obj) => {
   if('byr' in obj && 'iyr' in obj && 'eyr' in obj && 'hgt' in obj && 'hcl' in obj && 'ecl' in obj && 'pid' in obj) {
     return true;
@@ -86,14 +88,132 @@ const checkPassport = (obj) => {
   return false;
 }
 
+const reallyCheckPassport = (obj) => {
+  
+  //checks validity of birthyear
+  const byr = parseInt(obj.byr)
+  
+  if(byr < 1920 || byr > 2002) {
+  return false;
+  }
+
+  //checks validity of issue year
+  const iyr = parseInt(obj.iyr)
+  
+  if(iyr < 2010 || iyr > 2020) {
+  return false;
+  }
+
+  //checks validity of expiry year
+  const eyr = parseInt(obj.eyr)
+  
+  if(eyr < 2020 || eyr > 2030) {
+  return false;
+  }
+
+  // checks validity of height
+  const hgt = obj.hgt
+  const unit = hgt.slice(-2); 
+  
+  if (unit !== 'cm' && unit !== 'in') {
+    return false;
+  }
+  if (unit === 'cm') {
+    const hgtVal = parseInt(hgt.slice(0, -2))
+    
+    if (hgtVal < 150 || hgtVal > 193) {
+      return false;
+    }
+  }
+  if (unit === 'in') {
+    const hgtVal = parseInt(hgt.slice(0, -2))
+    
+    if (hgtVal < 59 || hgtVal > 76) {
+      return false;
+    }
+  }
+  // checks validity of hair color
+  const hcl = obj.hcl;
+  if (hcl[0] !== '#') {
+    return false;
+  }
+  for (let i = 1; i < 7; i++) {
+    const validChars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f']
+    
+    if (!hcl[i] || !validChars.includes(hcl[i])) {
+      return false;
+    }
+  }
+  // checks validity of eye color
+  const ecl = obj.ecl;
+  const validCodes = ['amb', 'blu', 'grn', 'gry', 'hzl', 'oth', 'brn']
+  if (!validCodes.includes(ecl)) {
+    return false;
+  }
+
+  // checks passport id
+  const pid = obj.pid;
+  if (pid.length !== 9) {
+    return false;
+  }
+  for (let char of pid) {
+    const validChars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    if (!validChars.includes(char)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const invalid = `
+
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+
+`
+
+const valid = `
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+
+`
+
+
+
+
 const validPassportCounter = (arr) => {
   let validPassports = 0;
   for (let passport of arr) { 
     if (checkPassport(passport)) {
-    validPassports++;
+      if (reallyCheckPassport(passport)) {
+        validPassports++;
+      }
     }
   }
   return validPassports;
 };
 
-console.log(validPassportCounter(passportsAsObjects))
+console.log(validPassportCounter(inputParser(valid)))
