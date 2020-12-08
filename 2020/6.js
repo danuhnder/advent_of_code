f03c6bee70ec624d865e4e102540b7e8fc617aa4
Daniel Robertson <danieljrobertson@gmail.com>
2020-12-07 21:02:57 -0500

    solve 6 part 1

--- 2020/6.js
+++ 2020/6.js
@@ -1,1 +1,40 @@
-const inputParser = (input) => JSON.parse(`[${input.replace(/\n/g, '@').split('@@').join(`"},{"`).split('@').join(`","`).split(' ').join(`","`).split(':').join(`":"`).slice(3, -3)}]`)
+
+
+const fullInput = require('./6data.js');
+const sampleInput = [`
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+`]
+
+const inputParser = (input) => input[0].split(/\n\s*\n/).map(str => str.replace(/\n/g, ""));
+
+
+const formCounter = arrayOfStrings => {
+  let sumItUp = 0;
+  
+  arrayOfStrings.forEach(str => {
+    const answers = [];
+    for(const char of str) {
+      if(!answers.includes(char)){
+        answers.push(char)
+      }
+    };
+    sumItUp += answers.length;
+  }); 
+  return sumItUp;
+}
+
+console.log(formCounter(inputParser(fullInput)));
